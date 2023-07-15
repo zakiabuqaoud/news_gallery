fcff8f15e4dc65fbf6e4d02d4a8241b44ba7d812
zakiabuqaoud <20172106@student.alazhar.edu.ps>
2023-07-14 16:30:38 -0800

    init commit part two

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,15 @@
+
+
+import 'dart:ui';
+
+// colors constant
+const Color constPrimaryColor = Color(0xffC44358);
+const Color constSecondaryColor = Color(0xff7CE217);
+
+//font sizes constant
+const int constTitlePageFontSize = 24;
+const int constNameFontSize = 24;
+const int constBodyFontSize = 18;
+const int constSmallFontSize = 16;
+const int constSmallestFontSize = 14;
+const int constButtonFontSize = 16;
